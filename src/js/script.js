e3ba96c880bb2c3017f808e643822931721c87ac
libra625 <18.12.17.moyo@gmail.com>
2024-10-22 22:55:03 +0300

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log('none');
+function getStringDescription(product) {
+    console.log("\u0422\u043E\u0432\u0430\u0440: ".concat(product.name, ", \u0426\u0456\u043D\u0430: ").concat(product.price, " \u0433\u0440\u043D., \u0412 \u043D\u0430\u044F\u0432\u043D\u043E\u0441\u0442\u0456: ").concat(product.available ? 'Так' : 'Ні'));
+}
+var product1 = { name: 'banana', price: 999, available: false };
+var product2 = { name: 'minion', price: 0, available: true };
+var product3 = { name: 'smth expensive', price: 99999999999, available: false };
+getStringDescription(product1);
+getStringDescription(product2);
+getStringDescription(product3);
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,1 +1,18 @@
-console.log('none')
+interface Product {
+    name: string;
+    price: number;
+    available: boolean;
+}
+
+function getStringDescription(product: Product) {
+    console.log(`Товар: ${product.name}, Ціна: ${product.price} грн., В наявності: ${product.available ? 'Так' : 'Ні'}`);
+    
+}
+
+const product1: Product = {name: 'banana', price: 999, available: false};
+const product2: Product = {name: 'minion', price: 0, available: true};
+const product3: Product = {name: 'smth expensive', price: 99999999999, available: false};
+
+getStringDescription(product1);
+getStringDescription(product2);
+getStringDescription(product3);
