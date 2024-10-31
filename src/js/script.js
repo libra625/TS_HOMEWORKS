566721326caeb80588ed394f12d6d945cb6d188a
libra625 <18.12.17.moyo@gmail.com>
2024-10-31 18:28:45 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-console.log('none');
+var reverseArray = function (arr) {
+    var len = arr.length;
+    for (var i = 0; i < len - 1; i++) {
+        if (i === len - i - 1)
+            break;
+        var temp = arr[i];
+        arr[i] = arr[len - i - 1];
+        arr[len - i - 1] = temp;
+    }
+    return arr;
+};
+var arr = [1, 2, 3, 4, 5, 6, 7];
+var reversedArr = reverseArray(arr);
+console.log(reversedArr);
+console.log(arr);
+console.log(reversedArr === arr);
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,1 +1,17 @@
-console.log('none')
+const reverseArray = function (arr: number[]): number[] {
+    const len: number = arr.length
+    for (let i: number = 0; i < len - 1; i++) {
+        if (i === len - i - 1) break
+        
+        const temp = arr[i]
+        arr[i] = arr[len - i - 1]
+        arr[len - i - 1] = temp
+    }
+    return arr;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7]
+const reversedArr = reverseArray(arr)
+console.log(reversedArr)
+console.log(arr)
+console.log(reversedArr === arr)
