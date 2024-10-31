848a88c2a0b936231ba47099676597cb7ce6d308
libra625 <18.12.17.moyo@gmail.com>
2024-10-31 18:11:47 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-console.log('none');
+var createTree = function (amount) {
+    if (amount === 0) {
+        return null;
+    }
+    //return 'sdds'
+    return {
+        value: amount,
+        child: createTree(amount - 1)
+    };
+};
+console.log(createTree(5));
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,1 +1,13 @@
-console.log('none')
+const createTree = function (amount: number): object | null {
+    if (amount === 0) {
+        return null
+    }
+    //return 'sdds'
+    return {
+        value: amount,
+        child: createTree(amount - 1)
+    }
+}
+
+console.log(createTree(5))
+
