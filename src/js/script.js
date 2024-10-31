752a1802e4e3323bd6b8cf8173c22c1152dea6fe
libra625 <18.12.17.moyo@gmail.com>
2024-10-31 19:24:29 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
-console.log('none');
+var wordsInput = document.createElement('input');
+wordsInput.placeholder = 'Введіть текст...';
+document.body.appendChild(wordsInput);
+var badWordsInput = document.createElement('input');
+badWordsInput.placeholder = 'Введіть заборонені слова через кому';
+document.body.appendChild(badWordsInput);
+var btnToCensore = document.createElement('button');
+btnToCensore.textContent = 'highlight';
+document.body.appendChild(btnToCensore);
+var censoredTextOutput = document.createElement('p');
+document.body.appendChild(censoredTextOutput);
+var highlightForbiddenWords = function (text, forbiddenWords) {
+    var result = text;
+    forbiddenWords.forEach(function (word) {
+        var regex = new RegExp("\\b".concat(word, "\\b"), 'gi');
+        result = result.replace(regex, "<del>".concat(word, "</del>"));
+    });
+    return result;
+};
+btnToCensore.addEventListener('click', function () {
+    var forbiddenWords = badWordsInput.value.split(',').map(function (word) { return word.trim(); });
+    censoredTextOutput.innerHTML = highlightForbiddenWords(wordsInput.value, forbiddenWords);
+});
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,1 +1,28 @@
-console.log('none')
+const wordsInput: HTMLInputElement = document.createElement('input')
+wordsInput.placeholder = 'Введіть текст...'
+document.body.appendChild(wordsInput);
+
+const badWordsInput: HTMLInputElement = document.createElement('input')
+badWordsInput.placeholder = 'Введіть заборонені слова через кому'
+document.body.appendChild(badWordsInput);
+
+const btnToCensore: HTMLButtonElement = document.createElement('button')
+btnToCensore.textContent = 'highlight'
+document.body.appendChild(btnToCensore);
+
+const censoredTextOutput: HTMLParagraphElement = document.createElement('p')
+document.body.appendChild(censoredTextOutput);
+
+const highlightForbiddenWords = function (text: string, forbiddenWords: string[]): string {
+    let result: string = text
+    forbiddenWords.forEach((word: string) => {
+        const regex = new RegExp(`\\b${word}\\b`, 'gi')
+        result = result.replace(regex, `<del>${word}</del>`)
+    })
+    return result
+}
+
+btnToCensore.addEventListener('click', () => {
+    const forbiddenWords: string[] = badWordsInput.value.split(',').map(word => word.trim())
+    censoredTextOutput.innerHTML = highlightForbiddenWords(wordsInput.value, forbiddenWords)
+})
