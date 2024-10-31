646322624735a9b4b4021f5bece138a982cc3e2e
libra625 <18.12.17.moyo@gmail.com>
2024-10-31 18:00:15 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
-console.log('none');
+function deepClone(obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    var clone = Array.isArray(obj) ? [] : {};
+    for (var key in obj) {
+        if (key in obj) {
+            clone[key] = deepClone(obj[key]);
+        }
+    }
+    return clone;
+}
+var original = { a: 1, b: { c: 2 }, d: [3, 4] };
+var copy = deepClone(original);
+console.log(copy);
+console.log(original !== copy);
+console.log(original.b !== copy.b);
+console.log(original.d !== copy.d);
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,1 +1,22 @@
-console.log('none')
+function deepClone<T>(obj: T): T {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    
+    const clone = Array.isArray(obj) ? [] : {};
+    
+    for (const key in obj) {
+        if (key in obj) {
+            (clone as T)[key] = deepClone((obj as T)[key]);
+        }
+    }
+    
+    return clone as T;
+}
+
+const original = {a: 1, b: {c: 2}, d: [3, 4]};
+const copy = deepClone(original);
+console.log(copy)
+console.log(original !== copy)
+console.log(original.b !== copy.b)
+console.log(original.d !== copy.d)
