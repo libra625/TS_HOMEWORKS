27ee23e6876e900422e768e7f6c476433e9e551e
libra625 <18.12.17.moyo@gmail.com>
2024-11-11 17:16:59 +0200

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
-console.log('none');
+var users = [
+    { id: 1, name: 'Alice', age: 25, isActive: true },
+    { id: 2, name: 'Bob', age: 30, isActive: false },
+    { id: 3, name: 'Charlie', age: 35, isActive: true },
+];
+function filterByProperty(array, property, value) {
+    var result = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][property] === value) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+var activeUsers = filterByProperty(users, 'isActive', true);
+console.log(activeUsers);
+// Результат: [
+//   { id: 1, name: 'Alice', age: 25, isActive: true },
+//   { id: 3, name: 'Charlie', age: 35, isActive: true }
+// ]
+var certainAgeUsers = filterByProperty(users, 'age', 35);
+console.log('\n', certainAgeUsers, '\n');
+var bobUsers = filterByProperty(users, 'name', 'Bob');
+console.log(bobUsers);
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,1 +1,39 @@
-console.log('none')
+type User = {
+    id: number;
+    name: string;
+    age: number;
+    isActive: boolean;
+};
+
+const users: User[] = [
+    {id: 1, name: 'Alice', age: 25, isActive: true},
+    {id: 2, name: 'Bob', age: 30, isActive: false},
+    {id: 3, name: 'Charlie', age: 35, isActive: true},
+];
+
+
+function filterByProperty<T, K extends keyof T>(array: T[], property: K, value: T[K]): T[] {
+    const result: T[] = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][property] === value) {
+            result.push(array[i]);
+        }
+    }
+    
+    return result;
+}
+
+
+const activeUsers = filterByProperty(users, 'isActive', true);
+console.log(activeUsers);
+// Результат: [
+//   { id: 1, name: 'Alice', age: 25, isActive: true },
+//   { id: 3, name: 'Charlie', age: 35, isActive: true }
+// ]
+const certainAgeUsers = filterByProperty(users, 'age', 35);
+console.log('\n', certainAgeUsers, '\n');
+
+
+const bobUsers = filterByProperty(users, 'name', 'Bob');
+console.log(bobUsers);
