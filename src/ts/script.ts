612a53cfc8fe09be808206a2cbf7e9891269d7fb
libra625 <18.12.17.moyo@gmail.com>
2025-03-09 00:11:41 +0200

    updated initial files script.js and script.ts

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,1 +1,8 @@
+import fs from "fs";
+
 console.log('none')
+
+fs.writeFile('example.txt', 'Hello, world!', (err: never) => {
+    if (err) throw err;
+    console.log('File has been written!');
+});
