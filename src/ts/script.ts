b2256e40665a5fb45d6837eb3ac5318839c891da
libra625 <18.12.17.moyo@gmail.com>
2025-03-09 00:27:54 +0200

    initial commit

--- src/ts/script.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-console.log('none')
-
-fs.writeFile('example.txt', 'Hello, world!', (err: never) => {
-    if (err) throw err;
-    console.log('File has been written!');
-});
